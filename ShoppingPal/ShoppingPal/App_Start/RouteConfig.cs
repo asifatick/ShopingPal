583d139e6531e0b00bb30f71cce3f59fcee5a891
Asif Atick <jossy81@gmail.com>
2013-02-19 23:04:36 +0600

    comit

---
+++ ShoppingPal/ShoppingPal/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ShoppingPal
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+  
+            //routes.MapRoute(
+            //    name: "magic",
+            //    url: "magic/{controller}/{action}/{id}",
+            //    defaults: new { controller = "Test", action = "UnRealTest", id = UrlParameter.Optional }
+            //);
+                    routes.MapRoute(
+              name: "Default",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+          );
+        }
+    }
+}
